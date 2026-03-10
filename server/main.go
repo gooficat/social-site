4f5e09c7cdbc97e5713315198dc27d1563fc1db2
Hasan Sabri <hsabri2007@gmail.com>
2026-03-10 15:09:46 +0000

    neat little router

--- server/main.go
+++ server/main.go
@@ -3,12 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
+	"path"
 )
 
+func ApiRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from the api\n")
+}
+func Routes(w http.ResponseWriter, r *http.Request) {
+	rt := strings.Split(r.URL.Path, "/")[1:]
+
+
+	switch rt[0] {
+	case "api":
+		ApiRoute(w, r)
+	default:
+		http.ServeFile(w, r, path.Join("../static", r.URL.Path))
+	}
+}
+
 func main() {
-	fmt.Println("Hello, World!")
-
-	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("../static"))))
-
+	fmt.Println("Hello from the server")
+	http.HandleFunc("/", Routes)
 	http.ListenAndServe(":3000", nil)
 }
