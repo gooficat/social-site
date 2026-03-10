ed13bb8c8f64795baeedd6d705d1d0bf7e9947df
Hasan Sabri <hsabri2007@gmail.com>
2026-03-10 15:29:26 +0000

    more on routing

--- server/main.go
+++ server/main.go
@@ -7,16 +7,38 @@ import (
 	"path"
 )
 
-func ApiRoute(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from the api\n")
+func ApiUserRoute(w http.ResponseWriter, r *http.Request, rt []string) {
+	if len(rt) < 1 {
+		fmt.Fprintf(w, "Hello from the user api\n")
+		return
+	}
+	switch rt[0] {
+	case "login":
+		fmt.Fprintf(w, "Login\n")
+	case "register":
+		fmt.Fprintf(w, "Register\n")
+	default:
+		fmt.Fprintf(w, "Unknown path\n")
+	}
+}
+
+func ApiRoute(w http.ResponseWriter, r *http.Request, rt []string) {
+	if len(rt) < 1 {
+		fmt.Fprintf(w, "Hello from the api\n")
+		return
+	}
+	switch rt[0] {
+	case "user":
+		ApiUserRoute(w, r, rt[1:])
+	default:
+		fmt.Fprintf(w, "Unknown path\n")
+	}
 }
 func Routes(w http.ResponseWriter, r *http.Request) {
 	rt := strings.Split(r.URL.Path, "/")[1:]
-
-
 	switch rt[0] {
 	case "api":
-		ApiRoute(w, r)
+		ApiRoute(w, r, rt[1:])
 	default:
 		http.ServeFile(w, r, path.Join("../static", r.URL.Path))
 	}
