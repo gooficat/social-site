4e2f181e7c9dc54a35af00304a7731344f76624a
Hasan Sabri <hsabri2007@gmail.com>
2026-03-14 15:10:03 +0000

    tweaks

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,8 @@ import router from 'bun-modular-router'
 Bun.serve({
 	port: 8080,
 	routes: router({
-		"/": () => new Response("Hello")
+		"/": () => new Response("Index"),
+		"/greet": () => new Response("Hello"),
+		"/meow": () => new Response("Meow"),
 	}),
 })
